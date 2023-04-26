eaaa521209cbe95ff0eafa66501dc0b6ee302d41
Bibek Parajuli <parajulibbek11@gmail.com>
2023-04-26 16:43:33 +0545

    assignment_1_Completed

---
+++ variables/q_2.dart
@@ -0,0 +1,6 @@
+ // 2. Declare a variable named myString and assign it a value of "Hello, world!". Print the value of the variable.
+
+void main(){
+  var myString = "Hello,world!";
+  print(myString);
+}
---
+++ variables/q_3.dart
@@ -0,0 +1,6 @@
+//  3. Declare a variable named myBoolean and assign it a value of true. Print the value of the variable.
+
+void main(){
+  bool myBoolean = true;
+  print(myBoolean);
+}
